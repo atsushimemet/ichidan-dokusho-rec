d1bfa589d573470bae5aabe34164930f3f9ac1de
atsushimemet <noap3b69n@gmail.com>
2025-08-22 15:21:28 +0900

    Add GA tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import ClientLayout from './layout-client';
 
@@ -14,6 +15,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-8YPNZ4VYKX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8YPNZ4VYKX');
+          `}
+        </Script>
+      </head>
       <body className="antialiased">
         <ClientLayout>
           {children}
